69df339b5f2114043c424a23fe5b1e1a6f9823c4
Andrew <70753182+IlinAndrey@users.noreply.github.com>
2021-04-04 23:41:34 +0300

    mda

---
+++ src/pr13/src/test/java/com/example/task13/Task13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task13;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
